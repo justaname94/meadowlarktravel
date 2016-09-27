0a3d75b5554c7636c129162c954c33cc4e06e34f
Roniel Valdez <roniel_valdez@outlook.com>
2016-09-26 20:40:47 -0400

    Handlebars testing

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,14 +1,61 @@
 var express = require('express');
-var handlebars = require('express-handlebars');
+var handlebars = require('express3-handlebars');
 var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-// Disable 'X-Powered-By response header'
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)'
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)'
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.comUS/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)'
+      }
+    ]
+  };
+}
+
+app.use(function(req, res, next) {
+  if(!res.locals.partials) {
+    res.locals.partials = {};
+  }
+  res.locals.partials.weather = getWeatherData();
+  next();
+});
+
+// Disable "X-Powered-By" response header
 app.disable('x-powered-by');
 
 // set up handlebars view engine
-app.engine('handlebars', handlebars({ defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) {
+        this._sections = {};
+      }
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
@@ -41,6 +88,23 @@ app.get('/tours/oregon-coast', function(req, res) {
 
 app.get('/tours/request-group-rate', function(req, res) {
   res.render('tours/request-group-rate');
+});
+
+app.get('/jquery-test', function(req, res) {
+  res.render('jquery-test');
+});
+
+app.get('/nursery-rhyme', function(req, res) {
+  res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res) {
+  res.json({
+    animal: 'squirrel',
+    bodyPart: 'tail',
+    adjective: 'bushy',
+    noun: 'heck'
+  });
 });
 
 // See Request Headers
