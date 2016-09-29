b38e0b4d0ce1d73bf0e93260dcd6158acfa5e271
Roniel Valdez <roniel_valdez@outlook.com>
2016-09-29 17:46:37 -0400

    update package.json

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -75,7 +75,7 @@ app.use(function(req, res, next) {
   // if there's a flash message, transfer
   // it to the context, then clear it
   res.locals.flash = req.session.flash;
-  delete req.session.flash();
+  delete req.session.flash;
   next();
 });
 
